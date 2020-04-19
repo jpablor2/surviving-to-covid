bed1813170e1ce213fbebd394dd78ed09dede036
jpablor2 <jpablor2@hotmail.com>
2020-04-18 21:38:07 -0600

    Adding java spring project

---
+++ surviving-to-covid/src/main/java/com/example/survivingtocovid/SurvivingToCovidApplication.java
@@ -0,0 +1,13 @@
+package com.example.survivingtocovid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurvivingToCovidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurvivingToCovidApplication.class, args);
+	}
+
+}
